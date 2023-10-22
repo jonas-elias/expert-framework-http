dd9f06e60168b25717739944f80e8cf60b73a516
Jonas Elias <jonasdasilvaelias@gmail.com>
2023-10-22 17:15:33 +0000

    feat: response emmiter

--- src/ResponseEmmiter.php
+++ src/ResponseEmmiter.php
@@ -10,7 +10,59 @@ namespace ExpertFramework\Http;
  */
 class ResponseEmmiter
 {
-    public function handle()
+    /**
+     * Method to send json echo
+     *
+     * @param Response $response
+     * @return string
+     */
+    public function sendJson(Response $response): string
     {
+        $this->sendHeaders($response->headers);
+        $this->sendStatus($response->statusCode);
+
+        echo $response->body;
+
+        return $response->body;
+    }
+
+    /**
+     * Method to send yaml echo
+     *
+     * @param Response $response
+     * @return string
+     */
+    public function sendYaml(Response $response): string
+    {
+        $this->sendHeaders($response->headers);
+        $this->sendStatus($response->statusCode);
+
+        echo $response->body;
+
+        return $response->body;
+    }
+
+    /**
+     * Method to send headers request
+     *
+     * @param array $headers
+     * @return void
+     */
+    private function sendHeaders(array $headers): void
+    {
+        foreach ($headers as $key => $header) {
+            header("{$key} {$header}; charset=utf-8");
+        }
+    }
+
+    /**
+     * Method to send status header
+     *
+     * @param int $statusCode
+     * @return void
+     */
+    private function sendStatus(int $statusCode): void
+    {
+        http_response_code($statusCode);
     }
 }
