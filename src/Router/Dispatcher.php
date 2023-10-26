98e8978d962e9fc02114890f9a1d674cde99f973
Jonas Elias <jonasdasilvaelias@gmail.com>
2023-10-26 15:39:40 +0000

    feat: status when error inject

--- src/Router/Dispatcher.php
+++ src/Router/Dispatcher.php
@@ -10,6 +10,7 @@ use ExpertFramework\Http\Exception\MethodNotAllowedException;
 use ExpertFramework\Http\Exception\NotFoundException;
 use ExpertFramework\Http\Middleware\DispatcherMiddleware;
 use ExpertFramework\Http\Request;
+use ExpertFramework\Http\Response;
 
 /**
  * class Dispatcher.
@@ -25,40 +26,65 @@ class Dispatcher
 
     /**
      * Method to dispatch handler.
+     *
+     * @param Request $request
+     * @param array $routes
+     *
+     * @return mixed
      */
-    public function handle(Request $request, array $routes)
+    public function handle(Request $request, array $routes): mixed
     {
-        (new DispatcherMiddleware())->handle();
+        try {
+            (new DispatcherMiddleware())->handle();
 
-        $method = $request->getMethod();
-        $uri = $request->path();
+            $method = $request->getMethod();
+            $uri = $request->path();
 
-        $listUri = explode('/', $uri);
-        $id = is_numeric(end($listUri)) ? (int) end($listUri) : null;
-        $uri = preg_replace('/\d+/', '{id}', $uri);
+            $listUri = explode('/', $uri);
+            $id = is_numeric(end($listUri)) ? (int) end($listUri) : null;
+            $uri = preg_replace('/\d+/', '{id}', $uri);
 
-        foreach ($routes as $pattern) {
-            if ($pattern['route'] === $uri) {
-                if ($pattern['method'] === $method) {
-                    $pattern = explode('@', $pattern['handler']);
-                    $controller = 'Jonaselias\\ExpertFramework\\Controller\\'.$pattern[0];
-                    $method = $pattern[1];
+            foreach ($routes as $pattern) {
+                if ($pattern['route'] === $uri) {
+                    if ($pattern['method'] === $method) {
+                        $pattern = explode('@', $pattern['handler']);
+                        $controller = 'Jonaselias\\ExpertFramework\\Controller\\' . $pattern[0];
+                        $method = $pattern[1];
 
-                    if (!$this->container) {
-                        $this->container = new Container();
+                        if (!$this->container) {
+                            $this->container = new Container();
+                        }
+
+                        return (new $controller($this->container))->$method($id ?? null);
                     }
 
-                    return (new $controller($this->container))->$method($id ?? null);
+                    $notAllowed = true;
                 }
+            }
 
-                $notAllowed = true;
+            if (isset($notAllowed)) {
+                throw new MethodNotAllowedException();
+            }
+
+            throw new NotFoundException();
+        } catch (MethodNotAllowedException $e) {
+            $statusCodeError = 403;
+        } catch (NotFoundException $e) {
+            $statusCodeError = 404;
+        } catch (\Exception $e) {
+            $statusCodeError = 500;
+        } finally {
+            if (isset($statusCodeError)) {
+                $response = new Response();
+
+                /**
+                 * evitando leaks de memória :)
+                 */
+                $status = $statusCodeError;
+                $statusCodeError = null;
+
+                return $response->status($status);
             }
         }
-
-        if (isset($notAllowed)) {
-            throw new MethodNotAllowedException();
-        }
-
-        throw new NotFoundException();
     }
 }
